9a595b12e6c0b9a77fbc5ead5313d794b4986d24
Filip-Papic <filippapic97@gmail.com>
2022-11-17 14:20:37 +0100

    draw custom shape, styling

--- script.js
+++ script.js
@@ -5,50 +5,60 @@ const config = document.createElement("div");
 config.classList.add("config");
 config.id = "config";
 
-// Properties
-const properties = document.createElement("div");
-properties.classList.add("properties");
+// Delay
+const delay = document.createElement("div");
 
-const delay = document.createElement("input");
-delay.classList.add("delay");
-delay.label = document.createElement("label");
-delay.label.innerHTML = "Delay (ms): 500";
-delay.type = "range";
-delay.min = 0;
-delay.max = 1000;
-delay.value = 500;
-delay.oninput = () => {
-  delay.label.innerHTML = `Delay (ms): ${delay.value}`;
+const delayInput = document.createElement("input");
+delayInput.classList.add("delay");
+delayInput.label = document.createElement("label");
+delayInput.label.innerHTML = "Delay (ms): 500";
+delayInput.type = "range";
+delayInput.min = 0;
+delayInput.max = 1000;
+delayInput.value = 500;
+delayInput.oninput = () => {
+  delayInput.label.innerHTML = `Delay (ms): ${delayInput.value}`;
 };
 
-const proportion = document.createElement("input");
-proportion.classList.add("proportion");
-proportion.label = document.createElement("label");
-proportion.label.innerHTML = "Proportion: 0.00";
-proportion.type = "range";
-proportion.min = 0.1;
-proportion.max = 0.99;
-proportion.value = 0.5;
-proportion.step = 0.01;
-proportion.oninput = () => {
-  proportion.label.innerHTML = `Proportion: ${proportion.value}`;
+delay.appendChild(delayInput.label);
+delay.appendChild(delayInput);
+
+// Proportion
+const proportion = document.createElement("div");
+
+const proportionInput = document.createElement("input");
+proportionInput.classList.add("proportion");
+proportionInput.label = document.createElement("label");
+proportionInput.label.innerHTML = "Proportion: 0.00";
+proportionInput.type = "range";
+proportionInput.min = 0.1;
+proportionInput.max = 0.99;
+proportionInput.value = 0.5;
+proportionInput.step = 0.01;
+proportionInput.oninput = () => {
+  proportionInput.label.innerHTML = `Proportion: ${proportionInput.value}`;
 };
 
-properties.appendChild(delay.label);
-properties.appendChild(delay);
-properties.appendChild(proportion.label);
-properties.appendChild(proportion);
-config.appendChild(properties);
+proportion.appendChild(proportionInput.label);
+proportion.appendChild(proportionInput);
 
-//Canvas
+config.appendChild(delay);
+config.appendChild(proportion);
+
+// Canvas
 const canvasHTML = document.createElement("canvas");
-//canvasHTML.width = 800;
-//canvasHTML.height = 600;
-canvasHTML.width = window.innerWidth * 0.99;
-canvasHTML.height = window.innerHeight-120;
+canvasHTML.width = window.innerWidth * 0.999;
+canvasHTML.height = window.innerHeight - 120;
 canvasHTML.role = "application";
 canvasHTML.tabIndex = 0;
 canvasHTML.ariaLabel = "Chaos Game";
+const customPoints = [];
+canvasHTML.addEventListener("click", (e) => {
+  customPoints.push({
+    x: e.offsetX,
+    y: e.offsetY,
+  });
+});
 const ctx = canvasHTML.getContext("2d");
 
 // Controls
@@ -58,8 +68,8 @@ control.classList.add("control");
 const start = document.createElement("button");
 start.classList.add("start");
 start.innerHTML = "Start";
-start.addEventListener("click", () => {
-  canvas.drawCustom();
+start.addEventListener("click", (e) => {
+  canvas.drawCustom(customPoints);
 });
 
 const finish = document.createElement("button");
@@ -96,7 +106,6 @@ container.appendChild(canvasHTML);
 container.appendChild(control);
 
 class Canvas {
-  points = [];
   constructor(canvas, ctx) {
     this.canvas = canvas;
     this.ctx = ctx;
@@ -122,16 +131,15 @@ class Canvas {
   drawPoint(x, y, color = "yellow", size = 2) {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, size, size);
-    this.points.push({ x: x, y: y });
   }
 
   drawTriangle() {
     const triangleCoords = [
-      { x: canvasHTML.width/2, y: canvasHTML.height * 0.1 },
+      { x: canvasHTML.width / 2, y: canvasHTML.height * 0.1 },
       { x: canvasHTML.width * 0.1, y: canvasHTML.height * 0.9 },
       { x: canvasHTML.width * 0.9, y: canvasHTML.height * 0.9 },
     ];
-    this.initJob(3, triangleCoords, proportion.value);
+    this.initJob(3, triangleCoords, proportionInput.value);
   }
 
   drawSquare() {
@@ -141,50 +149,40 @@ class Canvas {
       { x: canvasHTML.width * 0.9, y: canvasHTML.height * 0.9 },
       { x: canvasHTML.width * 0.1, y: canvasHTML.height * 0.9 },
     ];
-    this.initJob(4, squareCoords, proportion.value);
+    this.initJob(4, squareCoords, proportionInput.value);
   }
 
   drawHexagon() {
     const hexagonCoords = [
-      { x: canvasHTML.width/2, y: canvasHTML.height * 0.1 },
+      { x: canvasHTML.width / 2, y: canvasHTML.height * 0.1 },
       { x: canvasHTML.width * 0.9, y: canvasHTML.height * 0.3 },
       { x: canvasHTML.width * 0.9, y: canvasHTML.height * 0.7 },
-      { x: canvasHTML.width/2, y: canvasHTML.height * 0.9 },
+      { x: canvasHTML.width / 2, y: canvasHTML.height * 0.9 },
       { x: canvasHTML.width * 0.1, y: canvasHTML.height * 0.7 },
       { x: canvasHTML.width * 0.1, y: canvasHTML.height * 0.3 },
     ];
-    this.initJob(6, hexagonCoords, proportion.value);
+    this.initJob(6, hexagonCoords, proportionInput.value);
   }
 
-  drawCustom() {
-    const customCoords = this.points;
-    if (customCoords.length < 3) {
+  drawCustom(points) {
+    if (points.length < 3) {
       alert("Please add at least 3 points");
       return;
     }
-    this.initJob(customCoords.length, customCoords, proportion.value);
-  }
-
-  resetPoints() {
-    this.points = [];
+    this.initJob(points.length, points, proportionInput.value);
   }
 
   static download() {
     const link = document.createElement("a");
     link.download = `ChaosGame_${Date.now()}.png`;
-    link.href
-    = canvasHTML
-        .toDataURL("image/png")
-        .replace("image/png", "image/octet-stream");
+    link.href = canvasHTML
+      .toDataURL("image/png")
+      .replace("image/png", "image/octet-stream");
     link.click();
   }
 
   static clear() {
     ctx.clearRect(0, 0, canvasHTML.width, canvasHTML.height);
-  }
-
-  static random(points) {
-    return points[Math.floor(Math.random() * points.length)];
   }
 }
 
@@ -194,19 +192,19 @@ const canvas = new Canvas(canvasHTML, ctx);
 const shapes = document.createElement("div");
 
 const triangle = document.createElement("button");
-triangle.innerHTML = "🔺";
+triangle.innerHTML = "🔺Triangle";
 triangle.addEventListener("click", () => {
   canvas.drawTriangle();
 });
 
 const square = document.createElement("button");
-square.innerHTML = "🟥";
+square.innerHTML = "🟥Square";
 square.addEventListener("click", () => {
   canvas.drawSquare();
 });
 
 const hexagon = document.createElement("button");
-hexagon.innerHTML = "🔶";
+hexagon.innerHTML = "🍁Hexagon";
 hexagon.addEventListener("click", () => {
   canvas.drawHexagon();
 });
@@ -220,7 +218,7 @@ config.appendChild(shapes);
 const controller = new AbortController();
 
 function isDisabled(bool) {
-  proportion.disabled = bool;
+  proportionInput.disabled = bool;
   start.disabled = bool;
   finish.disabled = !bool;
   reset.disabled = bool;
@@ -233,6 +231,10 @@ class Point {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+  }
+
+  static random(points) {
+    return points[Math.floor(Math.random() * points.length)];
   }
 }
 
@@ -259,10 +261,10 @@ class Job {
     const pointCoordinates = this.getPointCoordinates();
     const resultPoints = [];
 
-    let randomStartPoint = Canvas.random(pointCoordinates);
+    let randomStartPoint = Point.random(pointCoordinates);
     let startPoint = new Point(randomStartPoint.x, randomStartPoint.y);
 
-    let randomTargetPoint = Canvas.random(
+    let randomTargetPoint = Point.random(
       pointCoordinates.filter((point) => {
         return point.x !== randomStartPoint.x && point.y !== randomStartPoint.y;
       })
@@ -291,6 +293,7 @@ class Job {
 
     this.resultPoints = resultPoints;
     isDisabled(false);
+    customPoints.length = 0;
   }
 
   async doRest(
@@ -301,7 +304,7 @@ class Job {
     resultPoints
   ) {
     for (let i = 0; i < Job.iterations(); i++) {
-      let randomPoint = Canvas.random(pointCoordinates);
+      let randomPoint = Point.random(pointCoordinates);
       startPoint = new Point(randomPoint.x, randomPoint.y);
 
       currentPoint.x =
@@ -316,7 +319,7 @@ class Job {
       if (finishClicked) {
         clearTimeout();
       } else {
-        await new Promise((resolve) => setTimeout(resolve, delay.value));
+        await new Promise((resolve) => setTimeout(resolve, delayInput.value));
       }
     }
     finishClicked = false;
